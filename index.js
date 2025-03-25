dbaa838c163f0dfbce4d8b64808846a8cdafbbe4
genilson-dev <programationwork@gmail.com>
2025-03-24 22:24:10 -0300

    Add comentario sobre o problema

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const saldacoes = nome => {
+// Crie uma função que receba um nome e retorne uma saudação na forma de uma string. Por exemplo, se o nome for "Genilson", a string de saudação deve ser "Bem-vindo(a), Genilson! Como posso te ajudar?."
+// Exemplo:
+const saldacoes = nome => { //
     console.log(`Bem-vindo(a), ${nome}! Como posso te ajudar?.`)
 }
 

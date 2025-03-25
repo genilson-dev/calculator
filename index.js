7e2dee08c3a5b50d0892d1a3b74a3cc75aae6570
genilson-dev <programationwork@gmail.com>
2025-03-24 22:19:19 -0300

    Add funcao basica de saldação

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-teste
+const saldacoes = nome => {
+    console.log(`Bem-vindo(a), ${nome}! Como posso te ajudar?.`)
+}
+
+const calledUser = "Genilson"
+saldacoes(calledUser) // Bem-vindo(a), Genilson! Como posso te ajudar?.
+
